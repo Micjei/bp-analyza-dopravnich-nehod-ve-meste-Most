9ea422034a1015b167371db6ef76bcbfc77a05ee
xbabus01 <xbabus01@fitbr.cz>
2025-02-15 18:50:01 +0100

    border fix, text color changed

--- components/FooterSection.tsx
+++ components/FooterSection.tsx
@@ -8,7 +8,7 @@ const FooterSection: React.FC<FooterProps> = ({ footerText }) => {
   return (
     <div
       className={
-        "absolute bottom-0 w-full flex flex-col items-center bg-[#C8E6C9] border-2 border-t-[#66BB6A] shadow-md text-[#388E3C] opacity-80 whitespace-nowrap"
+        "absolute bottom-0 w-full flex flex-col items-center bg-[#388E3C] border-2 border-[#66BB6A] shadow-md text-[#ffffff] opacity-80 whitespace-nowrap"
       }
     >
       <h3 className={"transition-opacity duration-300"}>{footerText}</h3>
--- components/HeaderSection.tsx
+++ components/HeaderSection.tsx
@@ -8,7 +8,7 @@ const HeaderSection: React.FC<FooterProps> = ({ headerText }) => {
   return (
     <div
       className={
-        "absolute top-0 w-full flex flex-col items-center p-5 bg-[#C8E6C9] border-2 border-b-[#66BB6A] shadow-md text-[#388E3C] opacity-80 whitespace-nowrap"
+        "absolute top-0 w-full flex flex-col items-center p-5 bg-[#66BB6A] border-2 border-[#66BB6A] shadow-md text-[#ffffff] opacity-80 whitespace-nowrap"
       }
     >
       <h3 className={"transition-opacity duration-300"}>{headerText}</h3>
